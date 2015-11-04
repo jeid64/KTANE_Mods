5eae53d01c0c717cc10999334a07d1638d5ddacd
Giovanni Frank Crisanti <frankcrisanti@aol.com>
2015-11-03 22:59:33 -0500

    added parts of morse module

--- Arduino.ino
+++ Arduino.ino
@@ -14,20 +14,34 @@ const int Keypad3ResultPin = 6;
 
 const int MazeResultPin = 5;
 
+unsigned int curFreq = 0;
+const int Freq0 = 4;
+const int Freq1 = 3;
+const int Freq2 = 2;
+const int Freq3 = 1;
+
+
 void setup() {
   // initialize serial:
   Serial.begin(9600);
-  
+
   //initalize pins
   pinMode(ButtonColorPinR, OUTPUT);     
   pinMode(ButtonColorPinG, OUTPUT);     
   pinMode(ButtonColorPinB, OUTPUT);     
   pinMode(ButtonSucessPin, OUTPUT);     
-  
+
   pinMode(Keypad0ResultPin, OUTPUT);     
   pinMode(Keypad1ResultPin, OUTPUT);     
   pinMode(Keypad2ResultPin, OUTPUT);     
   pinMode(Keypad3ResultPin, OUTPUT);     
+
+  pinMode(MazeResultPin, OUTPUT);     
+
+  pinMode(Freq0, OUTPUT);     
+  pinMode(Freq1, OUTPUT);     
+  pinMode(Freq2, OUTPUT);     
+  pinMode(Freq3, OUTPUT);     
 
   pinMode(A5, INPUT_PULLUP); // sets analog pin for input 
 }
@@ -39,47 +53,96 @@ void loop() {
   int buttonPressedCheck = analogToButton();
   //check to prevent noise, because fuck setting up shift registers
   if(buttonPressed == buttonPressedCheck){
-	  if(lastEvent!=1 && buttonPressed==1){
-		Serial.println("1button");
-		lastEvent = 1;
-	  }
-	  //big button depressed
-	  else if(lastEvent==1 && buttonPressed==0){
-		Serial.println("0button");
-		lastEvent = 2;
-	  }
-  
-	  if(lastEvent!=3 && buttonPressed==2){
-		Serial.println("0keypad");
-		lastEvent = 3;
-	  }else if(lastEvent!=4 && buttonPressed==3){
-		Serial.println("1keypad");
-		lastEvent = 4;
-	  }else if(lastEvent!=5 && buttonPressed==4){
-		Serial.println("2keypad");
-		lastEvent = 5;
-	  }else if(lastEvent!=6 && buttonPressed==5){
-		Serial.println("3keypad");
-		lastEvent = 6;
-	  }
-  
-	  if(lastEvent!=7 && buttonPressed==6){
-		Serial.println("1maze");
-		lastEvent = 7;
-	  }else if(lastEvent!=8 && buttonPressed==7){
-		Serial.println("3maze");
-		lastEvent = 8;
-	  }else if(lastEvent!=9 && buttonPressed==8){
-		Serial.println("2maze");
-		lastEvent = 9;
-	  }else if(lastEvent!=10 && buttonPressed==9){
-		Serial.println("0maze");
-		lastEvent = 10;
-	  }
-	  //allow buttons to be pressed in a row
-	  else if((lastEvent==7 || lastEvent==8 || lastEvent==9 || lastEvent==10) && buttonPressed==0){
-		lastEvent=0;
-	  }
+    if(lastEvent!=1 && buttonPressed==1){
+      Serial.println("1button");
+      lastEvent = 1;
+    }
+    //big button depressed
+    else if(lastEvent==1 && buttonPressed==0){
+      Serial.println("0button");
+      lastEvent = 2;
+    }
+
+    if(lastEvent!=3 && buttonPressed==2){
+      Serial.println("0keypad");
+      lastEvent = 3;
+    }
+    else if(lastEvent!=4 && buttonPressed==3){
+      Serial.println("1keypad");
+      lastEvent = 4;
+    }
+    else if(lastEvent!=5 && buttonPressed==4){
+      Serial.println("2keypad");
+      lastEvent = 5;
+    }
+    else if(lastEvent!=6 && buttonPressed==5){
+      Serial.println("3keypad");
+      lastEvent = 6;
+    }
+
+    if(lastEvent!=7 && buttonPressed==6){
+      Serial.println("1maze");
+      lastEvent = 7;
+    }
+    else if(lastEvent!=8 && buttonPressed==7){
+      Serial.println("3maze");
+      lastEvent = 8;
+    }
+    else if(lastEvent!=9 && buttonPressed==8){
+      Serial.println("2maze");
+      lastEvent = 9;
+    }
+    else if(lastEvent!=10 && buttonPressed==9){
+      Serial.println("0maze");
+      lastEvent = 10;
+    }
+    //allow buttons to be pressed in a row
+    else if((lastEvent==7 || lastEvent==8 || lastEvent==9 || lastEvent==10 || lastEvent==11 || lastEvent==12) && buttonPressed==0){
+      lastEvent=0;
+    }
+
+    else if(lastEvent!=11 && buttonPressed==10){
+      Serial.print(curFreq);
+      Serial.println("transmit");
+      lastEvent = 11;
+    }
+    else if(lastEvent!=12 && buttonPressed==11){
+      curFreq++;
+      if(curFreq==16){
+        curFreq = 0;
+      }
+
+      //rough conversion to binary
+      if(curFreq & B1000){
+        digitalWrite(Freq0, HIGH);
+      }
+      else{
+        digitalWrite(Freq0, LOW);
+      }
+
+      if(curFreq & B0100){
+        digitalWrite(Freq1, HIGH);
+      }
+      else{
+        digitalWrite(Freq1, LOW);
+      }
+
+      if(curFreq & B0010){
+        digitalWrite(Freq2, HIGH);
+      }
+      else{
+        digitalWrite(Freq2, LOW);
+      }
+
+      if(curFreq & B0001){
+        digitalWrite(Freq3, HIGH);
+      }
+      else{
+        digitalWrite(Freq3, LOW);
+      }
+
+      lastEvent = 12;
+    }
   }
 }
 
@@ -87,34 +150,48 @@ int analogToButton(){
   int analog = analogRead(5);
   for (int i=0; i < 4; i++)
   {
-     analog += analogRead(5);
+    analog += analogRead(5);
   }
   analog = analog / 5;
 
   if(analog>1000){
     return 0;
-  }else if(analog<20 && analog>10){
+  }
+  else if(analog<20 && analog>10){
     return 1;
-  }else if(analog<170 && analog>130){
+  }
+  else if(analog<170 && analog>130){
     return 2;
-  }else if(analog<270 && analog>230){
+  }
+  else if(analog<270 && analog>230){
     return 3;
-  }else if(analog<350 && analog>310){
+  }
+  else if(analog<350 && analog>310){
     return 4;
-  }else if(analog<410 && analog>370){
+  }
+  else if(analog<410 && analog>370){
     return 5;
-  }else if(analog<450 && analog>410){
+  }
+  else if(analog<450 && analog>410){
     return 6;
-  }else if(analog<490 && analog>450){
+  }
+  else if(analog<490 && analog>450){
     return 7;
-  }else if(analog<530 && analog>490){
+  }
+  else if(analog<530 && analog>490){
     return 8;
-  }else if(analog<560 && analog>530){
+  }
+  else if(analog<555 && analog>530){
     return 9;
-  }else if(analog<590 && analog>560){
+  }
+  else if(analog<570 && analog>555){
     return 10;
-  }else{
-	return 0;
+  }
+  else if(analog<580 && analog>570){
+    return 11;
+  }
+  else{
+    return 0;
   }
 }
 
@@ -130,54 +207,54 @@ void serialEvent() {
     // get the new byte:
     inputData = (byte)Serial.read(); 
   }
-  
+
   switch(lastEvent) {
-    case 1:
-     //light up button's color
-     handleColor(inputData);
-     break;
-    case 2:
-     //turn off color led
-     analogWrite(ButtonColorPinR, 0);
-     analogWrite(ButtonColorPinG, 0);
-     analogWrite(ButtonColorPinB, 0); 
-     //light up button sucess state
-     if(inputData==0){
-       digitalWrite(ButtonSucessPin, HIGH);
-       delay(1000);
-       digitalWrite(ButtonSucessPin, LOW);
-     }
-     break;
-   case 3:
-     if(inputData==1){
-       digitalWrite(Keypad0ResultPin, HIGH);
-     }
-     break;
-   case 4:
-     if(inputData==1){
-       digitalWrite(Keypad1ResultPin, HIGH);
-     }
-     break;
-   case 5:
-     if(inputData==1){
-       digitalWrite(Keypad2ResultPin, HIGH);
-     }
-     break;
-   case 6:
-     if(inputData==1){
-       digitalWrite(Keypad3ResultPin, HIGH);
-     }
-     break;
-   case 7:
-   case 8:
-   case 9:
-   case 10:
-     if(inputData==1){
-       digitalWrite(MazeResultPin, HIGH);
-       delay(1000);
-       digitalWrite(MazeResultPin, LOW);
-     }
-     break;
+  case 1:
+    //light up button's color
+    handleColor(inputData);
+    break;
+  case 2:
+    //turn off color led
+    analogWrite(ButtonColorPinR, 0);
+    analogWrite(ButtonColorPinG, 0);
+    analogWrite(ButtonColorPinB, 0); 
+    //light up button sucess state
+    if(inputData==0){
+      digitalWrite(ButtonSucessPin, HIGH);
+      delay(1000);
+      digitalWrite(ButtonSucessPin, LOW);
+    }
+    break;
+  case 3:
+    if(inputData==1){
+      digitalWrite(Keypad0ResultPin, HIGH);
+    }
+    break;
+  case 4:
+    if(inputData==1){
+      digitalWrite(Keypad1ResultPin, HIGH);
+    }
+    break;
+  case 5:
+    if(inputData==1){
+      digitalWrite(Keypad2ResultPin, HIGH);
+    }
+    break;
+  case 6:
+    if(inputData==1){
+      digitalWrite(Keypad3ResultPin, HIGH);
+    }
+    break;
+  case 7:
+  case 8:
+  case 9:
+  case 10:
+    if(inputData==1){
+      digitalWrite(MazeResultPin, HIGH);
+      delay(1000);
+      digitalWrite(MazeResultPin, LOW);
+    }
+    break;
   }
 }
 
@@ -209,3 +286,4 @@ void handleColor(byte inputData)
   }
   //color(75,75,0); // turn the RGB LED green  
 }
+
